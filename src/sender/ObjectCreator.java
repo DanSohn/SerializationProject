b94dfe5a03219eba65832d0b1748700ff198ec52
Daniel <chiefgustavo000@gmail.com>
2019-11-11 22:44:33 -0700

    Created basic text-based system to get user input of which object class they would like to create

--- src/sender/ObjectCreator.java
+++ src/sender/ObjectCreator.java
@@ -5,18 +5,29 @@ or more objects from a selection of objects using some sort of text-based menu s
 
 package sender;
 
+import java.util.Scanner;
+
 public class ObjectCreator {
 
     private static int userMenu(){
+
+        Scanner input = new Scanner(System.in);
         System.out.println("****************************************");
         System.out.println("******         User Menu          ******");
         System.out.println("****************************************");
+        System.out.println("******      Select An Option      ******");
+        System.out.println("1) Simple object with only primitives");
+        System.out.println("2) Object with reference to another object");
+        System.out.println("3) Object containing array of primitives");
+        System.out.println("4) Object containing array of object references");
+        System.out.println("5) Object using Java Collection's Stack to reference other objects");
+        System.out.println("I choose:");
 
-
-        return 0;
+        return input.nextInt();
     }
     public static void main(String[] args){
 
         int choice = userMenu();
+        System.out.println(choice);
     }
 }
