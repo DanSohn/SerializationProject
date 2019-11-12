cc63ed27556018428d2d27c703b4a8bac0d9a305
Daniel <chiefgustavo000@gmail.com>
2019-11-11 20:40:35 -0700

    Added all 5 object files. Initialized ObjectA

---
+++ sender/ObjectCreator.java
@@ -0,0 +1,22 @@
+/*
+1) A simple object with only primitives for instance variables. The user of your program must also be
+able to set the values for these fields.
+2) An object that contains references to other objects. Of course, these other objects must also be
+created at the same time, and their primitive instance variables must be settable by the user. Your
+program must also be able to deal with circular references (i.e. objects connected in a graph).
+3) An object that contains an array of primitives. Allow the user to set the values for the array
+elements to arbitrary values.
+4) An object that contains an array of object references. The other objects must also be created at the
+same time.
+5) An object that uses an instance of one of Java’s collection classes to refer to several other objects.
+These objects, too, must be created at the same time.
+ */
+
+public class ObjectCreator {
+
+
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ sender/src/ObjectA.java
@@ -0,0 +1,21 @@
+/*
+1) A simple object with only primitives for instance variables. The user of your program must also be
+able to set the values for these fields.
+ */
+
+public class ObjectA{
+    public int A;
+    public float B;
+    private boolean C;
+    protected char D;
+    private double E;
+
+    public ObjectA(int A, float B, boolean C, char D, double E){
+        this.A = A;
+        this.B = B;
+        this.C = C;
+        this.D = D;
+        this.E = E;
+    }
+    
+}
