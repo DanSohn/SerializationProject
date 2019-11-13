06faed7439078e7384e9ed5c25fe9ef5cfdbc43e
Daniel <chiefgustavo000@gmail.com>
2019-11-13 13:22:00 -0700

    Added server portion, receiving the object

--- src/receiver/Server.java
+++ src/receiver/Server.java
@@ -1,12 +1,22 @@
 package receiver;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
     public static void main(String[] args){
-
+        try{
+            int port = 4444;
+            ServerSocket serverSocket = new ServerSocket(port);
+            Socket socket = serverSocket.accept();
+            ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+            Object object = inputStream.readObject();
+            socket.close();
+        }catch(IOException | ClassNotFoundException e){
+            e.printStackTrace();;
+        }
     }
 }
